f46e088f643b7e37d127fa94b5c9b9954cbed267
khml <43922054+khml@users.noreply.github.com>
2019-09-21 13:50:06 +0900

    implement Board

---
+++ headers/Board.hpp
@@ -0,0 +1,29 @@
+//
+// Created by KHML on 2019-09-15.
+//
+
+#ifndef SANMOKU_BOARD_HPP
+#define SANMOKU_BOARD_HPP
+
+#include "BoardCore.hpp"
+
+
+namespace sanmoku
+{
+
+    class Board : public core::BoardCore
+    {
+    public:
+        Board();
+
+        Board(const Board& org);
+
+        virtual ~Board();
+
+        void printBoard();
+    };
+
+}
+
+
+#endif //SANMOKU_BOARD_HPP
